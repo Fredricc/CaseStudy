37cf0d1e828cc5b49383c1a81477da2a352fef47
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-22 20:06:54 +0300

    Call methods from the .NET Class Library using C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result = 3 + 1 * 5 / 2;
+            //Console.WriteLine(result);
+            //Console.WriteLine(5 / 10);
+            decimal gradePointAverage = 3.99872831m;
+            Console.WriteLine((int)gradePointAverage);
+            
+        }
+    }
+}
