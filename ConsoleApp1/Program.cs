6ea13472a657702d8b7d914b34757a834413ab8a
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-26 00:42:54 +0300

    Random Dice Next method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,12 +5,16 @@
         static void Main(string[] args)
         {
 
-            int result = 3 + 1 * 5 / 2;
+            //int result = 3 + 1 * 5 / 2;
             //Console.WriteLine(result);
             //Console.WriteLine(5 / 10);
-            decimal gradePointAverage = 3.99872831m;
-            Console.WriteLine((int)gradePointAverage);
-            
+            //decimal gradePointAverage = 3.99872831m;
+            //Console.WriteLine((int)gradePointAverage);
+
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+
         }
     }
 }
