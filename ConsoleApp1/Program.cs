3605a7d7cc0928789b57f58fe5dd807ca8e4c8f1
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-28 02:36:48 +0300

    code that generates three random numbers and displays them in output

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,22 +5,32 @@
         static void Main(string[] args)
         {
 
+            //Random dice = new Random();
+            //int roll1 = dice.Next();
+            //int roll2 = dice.Next(101);
+            //int roll3 = dice.Next(50, 101);
+
+            //int firstValue = 500;
+            //int secondValue = 600;
+            //int largerValue;
+
+            //largerValue = Math.Max(firstValue, secondValue);
+
+            //Console.WriteLine(largerValue);
+
+            //Console.WriteLine($"First roll: {roll1}");
+            //Console.WriteLine($"Second roll: {roll2}");
+            //Console.WriteLine($"Third roll: {roll3}");
+
             Random dice = new Random();
-            int roll1 = dice.Next();
-            int roll2 = dice.Next(101);
-            int roll3 = dice.Next(50, 101);
 
-            int firstValue = 500;
-            int secondValue = 600;
-            int largerValue;
+            int roll1 = dice.Next(1, 7);
+            int roll2 = dice.Next(1, 7);
+            int roll3 = dice.Next(1, 7);
 
-            largerValue = Math.Max(firstValue, secondValue);
+            int total = roll1 + roll2 + roll3;
 
-            Console.WriteLine(largerValue);
-
-            Console.WriteLine($"First roll: {roll1}");
-            Console.WriteLine($"Second roll: {roll2}");
-            Console.WriteLine($"Third roll: {roll3}");
+            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
         }
     }
