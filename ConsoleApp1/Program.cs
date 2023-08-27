77424276416fff7029ef0467a52513b9f9fb5746
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-28 01:53:33 +0300

    Math class and Max method.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,14 @@
             int roll2 = dice.Next(101);
             int roll3 = dice.Next(50, 101);
 
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue;
+
+            largerValue = Math.Max(firstValue, secondValue);
+
+            Console.WriteLine(largerValue);
+
             Console.WriteLine($"First roll: {roll1}");
             Console.WriteLine($"Second roll: {roll2}");
             Console.WriteLine($"Third roll: {roll3}");
